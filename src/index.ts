b835f427e62f5631fbe74074b1ce373bb8440aac
maishurong <maishurong@coding.net>
2020-09-13 17:00:47 +0800

    init project

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,51 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+console.log('---- start ----');
 
-import confetti from 'canvas-confetti';
+export const dbRequest = indexedDB.open('test', 1);
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+dbRequest.onsuccess = (event) => {
+    console.log('success: ');
+    // console.log((event.target as IDBOpenDBRequest).result);
+    // console.log(dbRequest.result);
+    const db = dbRequest.result;
+
+    db.onversionchange = () => {
+        db.close();
+        console.warn('db version change');
+    }
+
+    const transaction = db.transaction('books', 'readwrite');
+
+    const books = transaction.objectStore('books');
+
+    const book = {
+        id: 'js',
+        price: 10,
+        created: Date.now()
+    };
+
+    const request = books.add(book);
+
+    request.onsuccess = () => {
+        console.log('book added to the store', request.result);
+    }
+
+    request.onerror = () => {
+        console.error('book added error', request.error);
+    }
+};
+
+dbRequest.onerror = () => {
+    console.error('数据库打开报错');
+};
+
+dbRequest.onupgradeneeded = () => {
+    // const db = event?.target?.result;
+    // console.log(db);
+    console.log('onupgradeneeded');
+
+    const db = dbRequest.result;
+
+    if(!db.objectStoreNames.contains('books')) {
+        db.createObjectStore('books', { keyPath: 'id' });
+    }
+}
